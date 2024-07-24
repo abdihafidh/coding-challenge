0dcfc6e153762edfc7215fb28ba9d2a5a4bdf383
abdihafidh <144230998+abdihafidh@users.noreply.github.com>
2024-07-24 12:42:45 +0300

    Add files via upload

---
+++ Array reverse and sorrt.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Array Reverse and Sort
+        string[] pallets = { "B14", "A11", "B12", "A13" };
+        Console.WriteLine("Original array:");
+        foreach (string pallet in pallets)
+        {
+            Console.WriteLine($"--{pallet}");
+        }
+
+        Console.WriteLine("Sorted....");
+        Array.Sort(pallets);
+        foreach (string pallet in pallets)
+        {
+            Console.WriteLine($"--{pallet}");
+        }
+
+        Console.WriteLine("Reverse....");
+        Array.Reverse(pallets);
+        foreach (string pallet in pallets)
+        {
+            Console.WriteLine($"--{pallet}");
+        }
+
+        // Add values to the array
+        Array.Resize(ref pallets, pallets.Length + 2);
+        pallets[pallets.Length - 2] = "A15";
+        pallets[pallets.Length - 1] = "B16";
+
+        Console.WriteLine("Array after adding values:");
+        foreach (string pallet in pallets)
+        {
+            Console.WriteLine($"--{pallet}");
+        }
+
+        // Array Clear
+        Console.WriteLine("\nArray Clear:");
+        string[] pallets2 = { "B14", "A11", "B12", "A13" };
+        Console.WriteLine($"Before: {pallets2[0].ToLower()}");
+        Array.Clear(pallets2, 0, 2);
+        Console.WriteLine($"After: {(pallets2[0] != null ? pallets2[0].ToLower() : "null")}");
+        Console.WriteLine($"Clearing 2... count: {pallets2.Length}");
+        foreach (var pallet in pallets2)
+        {
+            Console.WriteLine($"-- {pallet}");
+        }
+    }
+}
---
+++ HashSet challenge.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(int[] A)
+    {
+        HashSet<int> abNumbers = new HashSet<int>();
+
+        A = new int[] { -5, -3, -1, 0, 3, 6 };
+        for (int i = 0; i < A.Length; i++)
+        {
+            abNumbers.Add(Math.Abs(A[i]));
+        }
+        return abNumbers.Count;
+    }
+}
+
+public class Solution
+{
+    public int solution(int X, int Y, int D)
+    {
+        X = 10;
+        Y = 85;
+        D = 30;
+        int jumpProgress = X;
+        int count = 0;
+        while (jumpProgress < Y)
+        {
+            jumpProgress += D;
+            count++;
+        }
+        return count;
+    }
+}
+
+public class Solution
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        var dummyList = new ListNode();
+        var head = dummyList;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val <= list2.val)
+            {
+                dummyList.next = list1;
+                dummyList = dummyList.next;
+                list1 = list1.next;
+            }
+            else
+            {
+                dummyList.next = list2;
+                dummyList = dummyList.next;
+                list2 = list2.next;
+            }
+        }
+        if (list1 != null)
+        {
+            dummyList.next = list1;
+        }
+        else if (list2 != null)
+        {
+            dummyList.next = list2;
+        }
+        return head.next;
+    }
+}
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        nums = new int[] { 2, 7, 11, 15 };
+        target = 9;
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
